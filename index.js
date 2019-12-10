f1eac24e5a102e046767b586193b1ee60ffc5944
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-12-09 22:52:52 -0500

    First commit.Server is up & running

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require('./server')
+
+const port = 7000;
+
+server.listen(port, () => {
+    console.log(`\n *** Server Running on http://localhost:${port} *** \n`)
+})
